0320ee87af958dbd22d39a246c73a19c0f55f1f4
Tatiana Mathia <mathiatatiana@gmail.com>
2024-05-02 01:47:26 +0300

    hooks/stste/events

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import React, {useState} from "react";
+
+/* eslint-disable no-unused-vars */
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [count, setCount] = useState(0);
+  
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  const appClass = isDarkMode ? "App dark" : "App light";
+  function handleDarkMode(){
+    setIsDarkMode((isDarkMode)=> !isDarkMode)
+  }
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkMode}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+  function handleClick(){
+    setIsInCart(!isInCart)
+  }
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick } className={isInCart? "remove" : "add" }>{isInCart?"Remove From Cart" :"Add to Cart"}</button>
     </li>
   );
 }
